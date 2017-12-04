c1848ed2bbdc308d2560b5b45bf7de3dbca0849f
Rohan Anand <thededlier@gmail.com>
2017-12-04 17:28:39 +0530

    check for empty params

--- shares/new_share.php
+++ shares/new_share.php
@@ -1,6 +1,9 @@
 <?php
+// error_reporting(0);
 // required headers
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET,POST");
 header("Content-Type: application/json; charset=UTF-8");
 
 // include database and object files
