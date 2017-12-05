894177a635a8e252756dbb63c0c2d2597e4c24aa
Rohan Anand <thededlier@gmail.com>
2017-12-05 11:27:54 +0530

    ideas api

--- ideas/new_idea.php
+++ ideas/new_idea.php
@@ -8,19 +8,19 @@ header("Content-Type: application/json; charset=UTF-8");
 
 // include database and object files
 include_once '../config/database.php';
-include_once '../objects/shares.php';
+include_once '../objects/ideas.php';
 
 // instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
 
 // initialize object
-$shares = new Shares($db);
+$shares = new Ideas($db);
 
 // check if more than 0 record found
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset(['user_id']) && isset($_POST['post_id') && isset($_POST['share_post_id'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id']) && isset($_POST['venue']) && isset($_POST['venue_address']) && isset($_POST['contact_name']) && isset($_POST['contact_number']) && isset($_POST['remarks'])) {
 
-    if($shares->newShare($_POST['user_id'], $_POST['post_id'], $_POST['share_post_id'])) {
+    if($shares->newIdea($_POST['user_id'], $_POST['venue'], $_POST['venue_address'], $_POST['contact_name'], $_POST['contact_number'], $_POST['remarks'])) {
         echo json_encode(
             array(
                 'status_code' => 200,
---
+++ objects/ideas.php
@@ -0,0 +1,40 @@
+<?php
+
+class Ideas {
+    // database connection and table name
+    private $conn;
+
+    // Object properties
+    public $id;
+    public $user_id;
+    public $venue;
+    public $address;
+    public $contact_name;
+    public $contact_number;
+    public $remarks;
+    public $timestamp;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function newIdea($user_id, $venue, $addres, $contact_name, $contact_number, $remarks) {
+        $this->user_id = htmlspecialchars(strip_tags($user_id));
+        $this->venue = htmlspecialchars(strip_tags($venue));
+        $this->address = htmlspecialchars(strip_tags($address));
+        $this->contact_name = htmlspecialchars(strip_tags($contact_name));
+        $this->contact_number = htmlspecialchars(strip_tags($contact_number));
+        $this->remarks = htmlspecialchars(strip_tags($remarks));
+
+        $sql = "INSERT INTO ideas(user_id, venue, address, contact_name, contact_number, remarks)
+                VALUES('$this->user_id', '$this->venue', '$this->address', '$this->contact_name', '$this->contact_number', '$this->remarks')";
+
+        if($this->conn->query($sql)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
