f91cef6ca8e91b89dc93e5493e44234e6a954353
Rohan Anand <thededlier@gmail.com>
2017-12-06 12:21:51 +0530

    user functions

---
+++ objects/users.php
@@ -0,0 +1,74 @@
+<?php
+
+class Users {
+    // database connection and table name
+    private $conn;
+
+    // Object properties
+    public $id;             // db id
+    public $fb_id;          // fb id of user
+    public $name;           // Users fb name
+    public $email;
+    public $mobile_number;
+    public $whatsapp_number;
+    public $city;
+    public $college;
+    public $address;
+    public $zipcode;
+    public $year_of_study;
+    public $status;
+    public $timestamp;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function newUser($fb_id, $name, $email, $mobile_number, $whatsapp_number, $city, $college, $address, $zipcode, $year_of_study, $status) {
+        $this->fb_id = htmlspecialchars(strip_tags($fb_id));
+        $this->name = htmlspecialchars(strip_tags($name));
+        $this->fb_id = htmlspecialchars(strip_tags($fb_id));
+        $this->mobile_number = htmlspecialchars(strip_tags($mobile_number));
+        $this->whatsapp_number = htmlspecialchars(strip_tags($whatsapp_number));
+        $this->city = htmlspecialchars(strip_tags($city));
+        $this->college = htmlspecialchars(strip_tags($college));
+        $this->address = htmlspecialchars(strip_tags($address));
+        $this->zipcode = htmlspecialchars(strip_tags($zipcode));
+        $this->year_of_study = htmlspecialchars(strip_tags($year_of_study));
+        $this->status = htmlspecialchars(strip_tags($status));
+
+        $sql = "INSERT INTO users(fb_id, name, email, mobile_number, whatsapp_number, city, college, address, zipcode, year_of_study, status)
+                VALUES(
+                    '$this->fb_id',
+                    '$this->name',
+                    '$this->email',
+                    '$this->mobile_number',
+                    '$this->whatsapp_number',
+                    '$this->city',
+                    '$this->college',
+                    '$this->address',
+                    '$this->zipcode',
+                    '$this->year_of_study',
+                    '$this->status'
+                )";
+
+        if($this->conn->query($sql)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function checkUserStatus($fb_id) {
+        $sql = "SELECT * FROM users WHERE fb_id = '$fb_id' AND status = 'set'";
+
+        $result = $this->conn->query($sql);
+
+        if($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
---
+++ users/new_user.php
@@ -0,0 +1,72 @@
+<?php
+// error_reporting(0);
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/users.php';
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$shares = new Users($db);
+
+// check if more than 0 record found
+if(
+    $_SERVER['REQUEST_METHOD'] == 'POST'
+    && isset($_POST['fb_id'])
+    && isset($_POST['name'])
+    && isset($_POST['email'])
+    && isset($_POST['mobile_number'])
+    && isset($_POST['whatsapp_number'])
+    && isset($_POST['city'])
+    && isset($_POST['college'])
+    && isset($_POST['address'])
+    && isset($_POST['zipcode'])
+    && isset($_POST['year_of_study'])
+) {
+
+    if($shares->newUser(
+        $_POST['fb_id'],
+        $_POST['name'],
+        $_POST['email'],
+        $_POST['mobile_number'],
+        $_POST['whatsapp_number'],
+        $_POST['city'],
+        $_POST['college'],
+        $_POST['address'],
+        $_POST['zipcode'],
+        $_POST['year_of_study']
+    )) {
+        echo json_encode(
+            array(
+                'status_code' => 200,
+                'message' => 'Accepted request'
+            )
+        );
+    } else {
+        echo json_encode(
+            array(
+                'status_code' => 500,
+                'message' => 'Internal server error'
+            )
+        );
+    }
+
+}
+
+else{
+    echo json_encode(
+        array(
+            'status_code' => 400,
+            'message' => 'BAD REQUEST. Invalid method or missing params'
+        )
+    );
+}
+?>
