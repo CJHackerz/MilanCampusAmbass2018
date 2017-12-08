2cf9101522eb78c5fd7b2892733bf3a52dd705e1
Rohan Anand <thededlier@gmail.com>
2017-12-08 15:35:34 +0530

    root b

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ class Database{
     private $host = "localhost";
     private $db_name = "milancr";
     private $username = "root";
-    private $password = "";
+    private $password = "srmdsa";
     public $conn;
 
     // get the database connection
