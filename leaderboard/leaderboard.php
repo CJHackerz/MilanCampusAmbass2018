a676abecccdafe8f306fb3b383d92841b148cc0c
Vimarsh Trehan <stellhawk1@gmail.com>
2017-12-09 00:24:14 +0530

    leaderboard improved

--- leaderboard/leaderboard.php
+++ leaderboard/leaderboard.php
@@ -8,33 +8,43 @@ header("Content-Type: application/json; charset=UTF-8");
 
 // include database and object files
 include_once '../config/database.php';
-include_once '../objects/users.php';
+include_once '../objects/shares.php';
 
-public function leader(){
+function leader(){//ignore the comments please
     // instantiate database and product object
     $database = new Database();
     $db = $database->getConnection();
 
-    $result = $db->query('SELECT id,fb_id,name,status FROM users ORDER BY status desc');
+    //$result = $db->query('SELECT id,fb_id,name,status FROM users ORDER BY status desc');
+    $result = $db->query('SELECT id, fb_id, name, count(*) as score from shares group by id order by count(*) desc');
 
     $list = array ();
     $x = 0;
-    while ($row = $result->fetch_array(MYSQLI_ASSOC) {
+
+    echo '<table><tr><th>Name</th><th>Facebook ID</th><th>Score</th></tr>';
+
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        echo '<tr><td>' . $row->name . '</td>';
+        echo '<td>' . $row->fb_id . '</td>';
+        echo '<td>' . $row->score . '</td></tr>';
+        /*
         $list[$row->id]['fb_id'] = $row->fb_id;
         $list[$row->id]['name'] = $row->name;
-        $list[$row->id]['status'] = $row->status;
+        $list[$row->id]['score'] = $row->score;
         $x++;
+        */
         if($x==10){
             break;
         }
     }
-
-// not sure which one is correct
+    echo '</table>';
+    /*
     foreach($list['id'] as $list) {
         echo $list['fb_id'], '<br>';
         echo $list['name'], '<br>';
         echo $list['status'], '<br>';
     }
+    */
     //echo $list;
 }
 ?>
